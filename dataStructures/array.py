8c8c0e4993473d81f9eae4305c3a48a1e1017843
nlemast <nlemaster47@gmail.com>
2020-08-16 17:15:14 -0400

    Create pop method

--- dataStructures/array.py
+++ dataStructures/array.py
@@ -6,3 +6,10 @@ class Array:
 	def push(self, element):
 		self.data[self.length] = element
 		self.length += 1
+
+	def pop(self):
+		self.length -= 1
+		last_element = self.data[self.length]
+		del self.data[self.length]
+
+		return last_element
