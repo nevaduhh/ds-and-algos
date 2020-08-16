29cd32196faff8f1bb98a40a4f4c697cf91e92c4
nlemast <nlemaster47@gmail.com>
2020-08-16 17:06:19 -0400

    Create push method on Array class

--- dataStructures/array.py
+++ dataStructures/array.py
@@ -1,4 +1,8 @@
 class Array:
-	def __init__(self, data):
-		self.data = data
+	def __init__(self):
+		self.data = {}
 		self.length = 0
+
+	def push(self, element):
+		self.data[self.length] = element
+		self.length += 1
