a6c34cbc791af2ef7ddb1c8ca52656d5bc599052
nlemast <nlemaster47@gmail.com>
2020-08-22 17:29:44 -0400

    Add dequeue method to Queue class

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -7,3 +7,16 @@ class Queue():
 		self.data[self.length] = element
 		self.length += 1
 
+	def dequeue(self):
+		new_data = {}
+		first_element = self.data[0]
+		del self.data[0]
+
+		for key, value in self.data.items():
+			new_key = int(key) - 1
+			new_data[new_key] = value
+
+		self.data = new_data
+		self.length -= 1
+
+		return first_element
