466fe473d28b42c5514885208b9e38f58fd14ffa
nlemast <nlemaster47@gmail.com>
2021-03-11 13:34:00 -0500

    Fib sequence with recursion and recursion + memoization

--- algorithms/fibSequence.js
+++ algorithms/fibSequence.js
@@ -6,22 +6,54 @@
 
 // iterative approach O(n)
 const fibSequence = (n) => {
-  let index = 1;
   let first = 0;
   let second = 1;
-  let newSum = 0;
+  let temp = 0;
 
-  while (index < n) {
-    newSum = first + second;
-    first = second;
-    second = newSum;
+  while (n >= 0) {
+    temp = first;
+    first = first + second;
+    second = temp;
 
-    index++;
+    n--;
   }
 
-  return newSum;
+  return second;
 }
 
 const result = fibSequence(6);
 
-console.log(result);
+console.log(result)
+
+// memoized approach
+const fibSequence2 = (n, index = 0, cache = {}) => {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  return fibSequence2(n - 1) + fibSequence2(n - 2);
+}
+
+const result2 = fibSequence2(6);
+
+console.log(result2);
+
+const fibSequence3 = (n, cache = {}) => {
+  if (cache[n]) {
+    return cache[n]
+  }
+
+  if (n <= 1) {
+    return n;
+  }
+
+  const newNum = fibSequence3(n - 1, cache) + fibSequence3(n - 2, cache);
+
+  cache[n] = newNum;
+
+  return newNum;
+}
+
+const result3 = fibSequence3(6);
+
+console.log(result3);
