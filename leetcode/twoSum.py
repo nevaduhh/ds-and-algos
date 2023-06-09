f00e2eb236313d2888b39874572148df4cee2f8c
nlemast <nlemaster47@gmail.com>
2023-06-09 09:56:03 -0700

    optimize twoSum

--- leetcode/twoSum.py
+++ leetcode/twoSum.py
@@ -7,15 +7,30 @@
 from typing import List
 
 
-def two_sum(int_list: List[int], target) -> List[int]:
-    for index, num in enumerate(int_list):
-        sliced_list = int_list[index + 1:]
+class Solution:
+    def two_sum(self, int_list: List[int], target) -> List[int]:
+        for index, num in enumerate(int_list):
+            sliced_list = int_list[index + 1:]
 
-        for comparison_index, comparison_num in enumerate(sliced_list):
-            num_sum = num + comparison_num
+            for comparison_index, comparison_num in enumerate(sliced_list):
+                num_sum = num + comparison_num
 
-            if num_sum == target:
-                return [index, index + comparison_index + 1]
+                if num_sum == target:
+                    return [index, index + comparison_index + 1]
+
+    def two_sum_2(self, nums: List[int], target: int) -> List[int]:
+        past_nums = {}
+
+        for index, num in enumerate(nums):
+            difference = target - num
+
+            if difference in past_nums:
+                return [past_nums[difference], index]
+
+            past_nums[num] = index
 
 
-print(two_sum([7, 3, 2, 4], 7))
+solution = Solution()
+
+print(solution.two_sum([7, 3, 2, 4], 7))
+print(solution.two_sum_2([-3, 4, 3, 90], 0))
